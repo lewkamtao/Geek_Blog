3cbe39dcbaa72a32940798b07642e4cfdab0b78e
div@kamtao.com <div@kamtao.com>
2021-11-30 15:27:52 +0800

    feat readme

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    apps: [{
-        name: 'geek_blog',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start',
-        env: {
-            NODE_ENV: "production",
-            PORT: 3095
-        }
-    }]
-}
+  apps: [
+    {
+      name: "geek_blog",
+      exec_mode: "cluster",
+      instances: "max", // Or a number of instances
+      script: "./node_modules/nuxt/bin/nuxt.js",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3095 // 可修改 修改注意反向代理也要改
+      }
+    }
+  ]
+};
