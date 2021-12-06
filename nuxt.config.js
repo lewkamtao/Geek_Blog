dd0661b6bbb02ff815e94d60d72f3436a3fd3cc1
lewkamtao <div@kamtao.com>
2021-12-06 12:39:07 +0800

    ✨ feat:fancybox 主题

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,29 +10,29 @@ export default {
       {
         name: "viewport",
         content:
-          "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover, user-scalable=0"
-      }
+          "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover, user-scalable=0",
+      },
     ],
     link: [],
     script: [
       { src: "//at.alicdn.com/t/font_2717088_fiq9yisuhzq.js" },
       { src: "https://cdn.bootcdn.net/ajax/libs/jquery/3.3.1/jquery.min.js" },
       {
-        src:
-          "https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"
-      }
-    ]
+        src: "https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "@/assets/css/fancybox.css",
+    "@/assets/css/fancybox-theme.css",
     "@/assets/css/reset.css",
     "@/assets/css/github-markdown.min.css",
     "@/assets/css/github.min.css",
     "@/assets/css/tag-plugins.scss",
     "@/assets/css/code-block.scss",
-    { src: "@/assets/css/main.scss", lang: "scss" }
+    { src: "@/assets/css/main.scss", lang: "scss" },
   ],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -40,7 +40,7 @@ export default {
     "@/plugins/route",
     "@/plugins/v-img",
     "@/plugins/element-ui",
-    "@/plugins//vueMarkdown.js"
+    "@/plugins//vueMarkdown.js",
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -49,7 +49,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build"
+    "@nuxt/typescript-build",
   ],
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -57,7 +57,7 @@ export default {
     ["semantic-ui-vue/nuxt", { css: false }], // if you have your own semantic-ui styles
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
-    "cookie-universal-nuxt"
+    "cookie-universal-nuxt",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -68,13 +68,13 @@ export default {
   axios: {
     proxy: true,
     prefix: "/api/",
-    credentials: true
+    credentials: true,
     // See https://github.com/nuxt-community/axios-module#options
   },
   router: {
     scrollBehavior(to, from, savedPosition) {
       return { x: 0, y: 0 };
-    }
+    },
   },
   proxy: {
     "/api/": {
@@ -84,9 +84,9 @@ export default {
 
       pathRewrite: {
         "^/api/": "/",
-        changeOrigin: true
-      }
-    }
+        changeOrigin: true,
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -103,11 +103,11 @@ export default {
           "component",
           {
             libraryName: "element-ui",
-            styleLibraryName: "theme-chalk"
-          }
-        ]
-      ]
-    }
+            styleLibraryName: "theme-chalk",
+          },
+        ],
+      ],
+    },
   },
-  loading: "~/components/custom/Loading.vue"
+  loading: "~/components/custom/Loading.vue",
 };
