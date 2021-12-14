b05da43ab503a959b1651540ab100840bf257920
卢锦涛 <div@kamtao.com>
2021-12-14 21:52:15 +0800

    🌈 style:调整内容屏

--- plugins/code-highlight.js
+++ plugins/code-highlight.js
@@ -1,34 +1,43 @@
-import Vue from 'vue'
-import JQ from 'jquery'
+import Vue from "vue";
+import JQ from "jquery";
 // 代码高亮
 import hljs from "highlight.js";
 // 复制
-import Clipboard from "_clipboard@2.0.4@clipboard";
+import Clipboard from "clipboard";
 
-Vue.directive('code-highlight', (el) => {
-  const highlight = el.querySelectorAll('pre code');
+Vue.directive("code-highlight", (el) => {
+  const highlight = el.querySelectorAll("pre code");
   highlight.forEach((block) => {
-    hljs.highlightBlock(block)
+    hljs.highlightBlock(block);
     // 显示行号
-    block.innerHTML = "<ul><li>" + block.innerHTML.replace(/\n/g, "\n</li><li>") + "\n</li></ul>";
+    block.innerHTML =
+      "<ul><li>" +
+      block.innerHTML.replace(/\n/g, "\n</li><li>") +
+      "\n</li></ul>";
     /* 添加头 */
-    let language = 'bash'
+    let language = "bash";
     block.classList.forEach((className) => {
-      if (className.indexOf("language-") !== -1) language = className.split('-')[1]
-    })
-    let pre_head = document.createElement("div")
-    pre_head.classList.add('pre-head')
-    pre_head.innerHTML = "<p><span class='code-language'>" + language.toUpperCase() + "</span><span class='copy'><i class='tag icon'></i>复制</span></p>";
-    block.parentNode.insertBefore(pre_head, block)
-  })
+      if (className.indexOf("language-") !== -1)
+        language = className.split("-")[1];
+    });
+    let pre_head = document.createElement("div");
+    pre_head.classList.add("pre-head");
+    pre_head.innerHTML =
+      "<p><span class='code-language'>" +
+      language.toUpperCase() +
+      "</span><span class='copy'><i class='tag icon'></i>复制</span></p>";
+    block.parentNode.insertBefore(pre_head, block);
+  });
   // 复制操作
-  el.querySelectorAll('pre').forEach((item)=>{
-    JQ(item).find('.copy').click((e)=>{
-      const codeText = JQ(item).find('code').text();
-      copyContent("copy", codeText);
-    })
-  })
-})
+  el.querySelectorAll("pre").forEach((item) => {
+    JQ(item)
+      .find(".copy")
+      .click((e) => {
+        const codeText = JQ(item).find("code").text();
+        copyContent("copy", codeText);
+      });
+  });
+});
 
 function copyContent(className, content) {
   let clipboard = new Clipboard("." + className, {
