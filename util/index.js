842b7c026fe5f74abd9c5c93161a54f4e4afe825
XiaoXuxuy <22476705@qq.com>
2021-12-19 01:21:57 +0800

    refactor 定位滚动条

--- util/index.js
+++ util/index.js
@@ -112,12 +112,12 @@ function toScroll(number = 0, time) {
     return number;
   }
   const spacingTime = 20; // 设置循环的间隔时间  值越小消耗性能越高
-  let spacingInex = time / spacingTime; // 计算循环的次数
+  let spacingIndex = time / spacingTime; // 计算循环的次数
   let nowTop = document.body.scrollTop + document.documentElement.scrollTop; // 获取当前滚动条位置
-  let everTop = (number - nowTop) / spacingInex; // 计算每次滑动的距离
+  let everTop = (number - nowTop) / spacingIndex; // 计算每次滑动的距离
   let scrollTimer = setInterval(() => {
-    if (spacingInex > 0) {
-      spacingInex--;
+    if (spacingIndex > 0) {
+      spacingIndex--;
       this.toScroll(nowTop += everTop);
     } else {
       clearInterval(scrollTimer); // 清除计时器
