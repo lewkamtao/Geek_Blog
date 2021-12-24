c4a003a3c75c15b2edc9790e398bff0a2f710ba0
XiaoXuxuy <v_xuxu@baidu.com>
2021-12-24 21:07:25 +0800

    style 目录样式优化

--- components/detail/parts/Catalogue.vue
+++ components/detail/parts/Catalogue.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="catalogue part">
     <div class="main-title" style="margin-bottom: 12px">目录</div>
-    <div class="catalogue-list vditor-outline" id="outline"></div>
+    <div class="catalogue-scroll">
+      <ul class="catalogue-list"></ul>
+    </div>
   </div>
 </template>
 
@@ -12,12 +14,12 @@ export default {
   methods: {
     // 文章目录
     catalogue() {
-      document
-        .querySelector(".catalogue .catalogue-list")
-        .style.setProperty("max-height", "500px");
-      document
-        .querySelector(".catalogue .catalogue-list")
-        .style.setProperty("overflow", "auto");
+      // document
+      //   .querySelector(".catalogue .catalogue-list")
+      //   .style.setProperty("max-height", "500px");
+      // document
+      //   .querySelector(".catalogue .catalogue-list")
+      //   .style.setProperty("overflow", "auto");
       let contentElement = document.getElementById("article-editor");
       let catalogueContent = document.querySelector(
         ".catalogue .catalogue-list"
@@ -56,15 +58,17 @@ export default {
           // 设置目录层级样式
           let markCss = "mark-" + tag_name;
           document.querySelector(".catalogue .catalogue-list").innerHTML +=
-            '<div class="catalogue-item ' +
+            '<li class="catalogue-item ' +
             markCss +
             '" name="' +
             mark +
-            '" style="text-indent:' +
+            '">' +
+            '<a' +
+            ' style="text-indent:' +
             indent +
-            'em"># ' +
-            text +
-            "</div>";
+            'em">' +
+            text + '</a>' +
+            "</li>";
           document.querySelector(".catalogue").style.removeProperty("display");
         }
       });
@@ -79,7 +83,12 @@ export default {
             if (active !== null) {
               active.classList.remove("active");
             }
-            e.target.classList.add("active");
+            if (e.target.tagName.toUpperCase() === 'A') {
+              console.log(e.target.parentNode)
+              e.target.parentNode.classList.add("active");
+            } else {
+              e.target.classList.add("active");
+            }
             let offsetTop = document.querySelector(
               "#" + item.getAttribute("name")
             ).offsetTop;
@@ -112,23 +121,110 @@ export default {
 </script>
 <style lang="scss">
 .catalogue {
-  .catalogue-item {
-    padding: 0.5rem;
+  .catalogue-scroll {
+    padding-left: 7px;
+    max-height: 500px;
+    overflow: auto;
+  }
+
+  .catalogue-scroll::-webkit-scrollbar {
+    width: 3px;
+    height: 3px;
+  }
+
+  .catalogue-scroll::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0);
+  }
+
+  .catalogue-scroll:hover::-webkit-scrollbar-thumb {
+    background: rgba(146, 146, 146, .3);
+  }
+
+  .catalogue-scroll::-webkit-scrollbar-track {
+    background: none;
+  }
+
+  .catalogue-item a {
     cursor: pointer;
+    margin: 0 10px 0 10px;
+    display: block;
     border-radius: 4px;
-    line-height: 1.2;
+    padding: 0.5em;
   }
 
-  .catalogue-item.active {
+  .catalogue-item.active a {
     color: #000 !important;
-    font-weight: bolder;
+    //font-weight: bolder;
     background: #ededed;
   }
 
-  .catalogue-item:hover {
+  .catalogue-item a:hover {
     color: #000 !important;
-    font-weight: bolder;
+    //font-weight: bolder;
     background: #ededed;
+  }
+
+  .catalogue-list {
+    list-style: none;
+    position: relative;
+    border-left: 1px solid rgba(50, 50, 50, 0.06);
+    padding: 20px 0px 20px 0;
+  }
+
+  .catalogue-list::before {
+    bottom: auto;
+    top: 0;
+    position: absolute;
+    left: -6px;
+    width: 11px;
+    height: 11px;
+    border: 2px solid #14b596;
+    border-radius: 15px;
+    content: '';
+    transition: .15s;
+  }
+
+  .catalogue-list::after {
+    position: absolute;
+    bottom: 0;
+    left: -6px;
+    width: 11px;
+    height: 11px;
+    border: 2px solid #14b596;
+    border-radius: 15px;
+    content: '';
+    transition: .15s;
+  }
+
+  .catalogue-list li {
+    position: relative;
+    display: block;
+  }
+
+  .catalogue-list li::before {
+    top: 50%;
+    transform: translateY(-50%);
+    position: absolute;
+    bottom: 0;
+    left: -4px;
+    width: 7px;
+    height: 7px;
+    border: 2px solid #b1b1b1;
+    background: #b1b1b1;
+    border-radius: 15px;
+    content: '';
+    transition: .15s;
+  }
+
+  .catalogue-list li:hover::before {
+    border: 2px solid #14b596;
+    background: #14b596;
+  }
+
+  .catalogue-list li.active::before {
+    border: 2px solid #14b596;
+    background: #14b596;
   }
 
   .catalogue-list::-webkit-scrollbar {
